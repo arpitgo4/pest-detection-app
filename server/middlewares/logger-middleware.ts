d47b29d99cdfec3c59f4736b590334a1798ee385
Arpit Goyal <arpit.go4@gmail.com>
2019-04-10 14:34:02 +0530

    haeders logging in added in logger middleware

--- server/middlewares/logger-middleware.ts
+++ server/middlewares/logger-middleware.ts
@@ -3,7 +3,7 @@ import chalk from 'chalk';
 
 
 const loggerMiddleware = (err, req, res, next) => {
-    console.log(chalk.red(`[logger-middleware] Error logged: ${req.method} ${req.url} : body => ${JSON.stringify(req.body || {})}, error => ${err.message}, stack trace: ${console.trace()}`));
+    console.log(chalk.red(`[logger-middleware] Error logged: ${req.method} ${req.url} : body => ${JSON.stringify(req.body || {})}, headers: { x-token: ${req.headers['x-token']}, error => ${err.message}, stack trace: ${console.trace()}`));
     next(err);
 };
 
